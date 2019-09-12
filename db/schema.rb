db0a7df3c1e148be572308b9f79ec178c6a7732c
Nox Yu <nox19990209@gmail.com>
2019-09-12 16:12:44 -0400

    migration to create schema for models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_020639) do
+ActiveRecord::Schema.define(version: 2019_09_11_024719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "office_hours", force: :cascade do |t|
+    t.bigint "course_id"
+    t.string "day"
+    t.time "star"
+    t.time "end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_office_hours_on_course_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "course_id"
+    t.bigint "user_id"
+    t.text "description"
+    t.date "duedate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_tasks_on_course_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +56,8 @@ ActiveRecord::Schema.define(version: 2019_09_11_020639) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "courses", "users"
+  add_foreign_key "office_hours", "courses"
+  add_foreign_key "tasks", "courses"
+  add_foreign_key "tasks", "users"
 end
