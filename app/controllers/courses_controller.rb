efc7983797b94cb60f42dd9d7835832a97c74b8d
Nox Yu <nox19990209@gmail.com>
2019-09-13 15:53:28 -0400

    can add a course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   def index
     @courses = Course.all
