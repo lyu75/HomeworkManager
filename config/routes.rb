0e33c90729bce1ddfeacdc89bae5a41ed8cbf34d
Nox Yu <nox19990209@gmail.com>
2019-09-18 00:06:16 -0400

    setup oh controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'profile' => "pages#profile", as: "profile"
 
   resources :courses
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
